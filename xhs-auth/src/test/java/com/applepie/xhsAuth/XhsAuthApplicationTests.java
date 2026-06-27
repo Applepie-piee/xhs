c6c5e7fbd60a49c2cc7a1e13d02a74ca52470b41
lanme <lanmeiheiqiao@163.com>
2026-06-27 22:19:12 +0800

    Changes

---
+++ xhs-auth/src/test/java/com/applepie/xhsAuth/XhsAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.applepie.xhsAuth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XhsAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
